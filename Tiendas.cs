64a0ebc201159b4153cfd0153fd56dc88f179d32
jtomassilva <31058005+jtomassilva@users.noreply.github.com>
2017-10-08 17:25:35 -0300

    Add files via upload

---
+++ Tiendas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Tiendas
+    {
+        int area;
+        int nrotienda;
+        string nombre;
+        public List<Empleados> empleados;
+
+        public Tiendas(int area, string nombre)
+        {
+            this.area = area;
+            this.nombre = nombre;
+            empleados = new List<Empleados>();
+        }
+
+    }
+}
